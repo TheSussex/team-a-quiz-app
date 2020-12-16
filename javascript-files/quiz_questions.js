3a7e90e38e57a400bde22effb1d0a2aacf61f84c
debbymags <debbymags@gmail.com>
2020-12-16 19:57:12 +0100

    added script for quiz home

--- javascript-files/quiz_questions.js
+++ javascript-files/quiz_questions.js
@@ -1,5 +1,3 @@
-let question_1 = "Who is the governor of Central Bank of Nigeria?"
-
 var question1 = {
     number: 1,
     question: "Who is the governor of Central Bank of Nigeria?",
@@ -12,22 +10,45 @@ var question1 = {
 
 var question2 = {
     number: 2,
-    question: "Which team won the Premier League in 2001?",
-    option_a: "I don't know",
-    option_b: "Chelsea",
-    option_c: "Arsenal",
-    answer: "c",
+    question: "To 'THWART' is to:",
+    option_a: "Weaken",
+    option_b: "Make fun of",
+    option_c: "Threaten",
+    answer: "a",
+    image_url: "../assets/flowerpot.png"
+}
+var question3 = {
+    number: 3,
+    question: "Who is the coach of Manchester United Football Club?",
+    option_a: "Alex Ferguson",
+    option_b: "Ole Solskjaer",
+    option_c: "Jose Mourinho",
+    answer: "b",
     image_url: "../assets/football.png"
+}
+var question4 = {
+    number: 4,
+    question: "Obama is the ----- president?",
+    option_a: "47th",
+    option_b: "45th",
+    option_c: "44th",
+    answer: "c",
+    image_url: "../assets/honey_lines.png"
+}
+var question5 = {
+    number: 5,
+    question: "The capital of Madagascar is?",
+    option_a: "Monrovia",
+    option_b: "Maputo",
+    option_c: "Antananariva",
+    answer: "c",
+    image_url: "../assets/clothes_on_lines.png"
 }
 
 let user_answers = []
 let qnumber = 1;
 let score = 0;
 
-function setValues(event){
-    event.preventDefault()
-    // qnumber
-}
 
 
 function submitResponse(event) {
@@ -49,6 +70,10 @@ function submitResponse(event) {
     changeQuestion(qnumber+1)
     qnumber = qnumber + 1;
     console.log (qnumber)
+    if (qnumber==6){
+        computeResult()
+        return
+    }
     return
 }
 
@@ -60,29 +85,45 @@ function storeResponse(val){
 }
 
 function computeResult(){
-    if (user_answer[0] == question_1.answer){
+    if (user_answers[0] == question1.answer){
         score += 1
     }
-    if (user_answer[1] == question_2.answer){
+    if (user_answers[1] == question2.answer){
         score += 1
     }
-    if (user_answer[2] == question_3.answer){
+    if (user_answers[2] == question3.answer){
         score += 1
     }
-    if (user_answer[3] == question_4.answer){
+    if (user_answers[3] == question4.answer){
         score += 1
     }
-    if (user_answer[4] == question_5.answer){
+    if (user_answers[4] == question5.answer){
         score += 1
     }
     console.log(score)
-    outputScore(score)
+    let userName = sessionStorage.getItem("username")
+    storeScoreDetails(score, userName)
+    sessionStorage.setItem("score", score)
+    outputScore()
     return
 }
 
-function outputScore(score){
+function outputScore(){
     window.location.href = "result_page.html"
-    document.getElementById("result").innerHTML = score + "/5"
+    return
+}
+
+function changeScore(event){
+    event.preventDefault()
+    console.log(sessionStorage.getItem("score"))
+    if (!sessionStorage.getItem("score")){
+        scoreN = 0
+    }
+    else{
+        scoreN = sessionStorage.getItem("score")
+    }
+    document.getElementById("result").innerHTML = scoreN+ "/5"
+    return
 }
 
 function changeQuestion(number) {
@@ -92,7 +133,7 @@ function changeQuestion(number) {
         document.getElementById("option_a_label").innerHTML = question1.option_a
         document.getElementById("option_b_label").innerHTML = question1.option_b
         document.getElementById("option_c_label").innerHTML = question1.option_c
-        document.getElementById("html_").style.backgroundImage = "url("+question1.image_url+")";
+        document.getElementById("html_").style.backgroundImage = "url("+question1.image_url+")"
     }
     else if (number==2){
         document.getElementById("question_heading").innerHTML = "Question " + number
@@ -100,13 +141,111 @@ function changeQuestion(number) {
         document.getElementById("option_a_label").innerHTML = question2.option_a
         document.getElementById("option_b_label").innerHTML = question2.option_b
         document.getElementById("option_c_label").innerHTML = question2.option_c
-        document.getElementById("html_").style.backgroundImage = "url("+question2.image_url+")";
+        document.getElementById("html_").style.backgroundImage = "url("+question2.image_url+")"
     }
-    else if (number==3){}
-    else if (number==4){}
+    else if (number==3){
+        document.getElementById("question_heading").innerHTML = "Question " + number
+        document.getElementById("question_text").innerHTML = question3.question
+        document.getElementById("option_a_label").innerHTML = question3.option_a
+        document.getElementById("option_b_label").innerHTML = question3.option_b
+        document.getElementById("option_c_label").innerHTML = question3.option_c
+        document.getElementById("html_").style.backgroundImage = "url("+question3.image_url+")"
+    }
+    else if (number==4){
+        document.getElementById("question_heading").innerHTML = "Question " + number
+        document.getElementById("question_text").innerHTML = question4.question
+        document.getElementById("option_a_label").innerHTML = question4.option_a
+        document.getElementById("option_b_label").innerHTML = question4.option_b
+        document.getElementById("option_c_label").innerHTML = question4.option_c
+        document.getElementById("html_").style.backgroundImage = "url("+question4.image_url+")"
+    }
     else if (number==5){
-        computeResult()
+        document.getElementById("question_heading").innerHTML = "Question " + number
+        document.getElementById("question_text").innerHTML = question5.question
+        document.getElementById("option_a_label").innerHTML = question5.option_a
+        document.getElementById("option_b_label").innerHTML = question5.option_b
+        document.getElementById("option_c_label").innerHTML = question5.option_c
+        document.getElementById("html_").style.backgroundImage = "url("+question5.image_url+")"
     }
+    else{
+        return
+    }
+    return
+}
+
+function storeScoreDetails(score, userName){
+    let timeElapsed = Date.now()
+    let today = new Date(timeElapsed)
+    let date = today.toDateString()
+    let points = (score/5)*100
+
+    if (localStorage.getItem(userName)){
+        let user_details = JSON.parse(localStorage.getItem(userName))
+        user_details.push([date, score, points+"%"])
+        localStorage.setItem(userName, JSON.stringify(user_details))
+    }
+    else {
+        let userdetails = [[date, score, points+"%"]]
+        localStorage.setItem(userName, JSON.stringify(userdetails))
+    }
+
+    return
+}
+
+function goToDashboard(event) {
+    event.preventDefault()
+    window.location.href = "quiz_home.html"
+
+    if (!sessionStorage.getItem("username")){
+        window.location.href = "landing_page.html"
+        return;
+    }
+    return;
+}
+
+function fillTable(event){
+    event.preventDefault()
+
+    document.getElementById("welcome").innerHTML = sessionStorage.getItem("username") + ","
+    let user_key = sessionStorage.getItem("username")
+    let user_details = JSON.parse(localStorage.getItem(user_key))
+
+    for(i=0; i<user_details.length; i++){
+        var table = document.getElementById("scoreTable")
+        var row = table.insertRow(i+1)
+        var cell1 = row.insertCell(0)
+        var cell2 = row.insertCell(1)
+        var cell3 = row.insertCell(2)
+        var cell4 = row.insertCell(3)
+
+        cell1.innerHTML = i+1
+        cell2.innerHTML = user_details[i][0]
+        cell3.innerHTML = user_details[i][1]
+        cell4.innerHTML = user_details[i][2]
+    }
+    return
+}
+
+function goToQuestions(event){
+    event.preventDefault()
+
+    window.location.href = "question.html"
+    return
+}
+
+function goToHome(event){
+    event.preventDefault()
+
+    sessionStorage.clear()
+    window.location.href = "landing_page.html"
+    return
+}
+
+function signUserOut(event){
+    event.preventDefault()
+
+    sessionStorage.clear()
+    window.location.href = "landing_page.html"
     return
 }
 
